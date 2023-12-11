6b459e97fd361df1d540fb6bf066037f782cd899
Sly Cash <100027808@mvla.net>
2023-12-11 11:03:45 -0800

    gui not running timeline correctly

--- src/ElevatorSimController.java
+++ src/ElevatorSimController.java
@@ -15,6 +15,7 @@ import passengers.Passengers;
  */
 // TODO: Auto-generated Javadoc
 public class ElevatorSimController {
+	// Owner by Sly
 	
 	/**  Constant to specify the configuration file for the simulation. */
 	private static final String SIM_CONFIG = "ElevatorSimConfig.csv";
@@ -104,7 +105,7 @@ public class ElevatorSimController {
 		// floor 6 at 1
 		elevatorY = NUM_FLOORS - elevatorY; // inverts
 		elevatorY = elevatorY * 2 + 1; //adjusts to scale
-		gui.updateGUI(building.getElevatorState()[0], building.getElevatorState()[1], elevatorY, building.getElevatorState()[4], building.getWaitingPassengers());
+		gui.updateGUI(building.getElevatorState()[0], building.getElevatorState()[1], elevatorY, building.getElevatorState()[4], building.getWaitingPassengers(), stepCnt);
 	}
 	
 	/**
@@ -266,6 +267,7 @@ public class ElevatorSimController {
 			if (!gui.equals(null)) gui.endSimulation();
 			
 		}
+		System.out.println("stepSim");
 	}
 	
 	private void checkPassQueue() {
