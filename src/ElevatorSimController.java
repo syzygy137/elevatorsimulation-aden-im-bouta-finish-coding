e0a1d3621969ede3b5ad41e1a1e8b7eb82bde3d4
Dan Bita <100027001@mvla.net>
2023-12-12 11:31:47 -0800

    fixed null point exception

--- src/ElevatorSimController.java
+++ src/ElevatorSimController.java
@@ -280,6 +280,7 @@ public class ElevatorSimController {
 		boolean end = false;
 		ArrayList<Passengers> newPassengers = new ArrayList<Passengers>();
 		while (!end) {
+			if (passQ.isEmpty()) return;
 			if (passQ.peek().getTime() == stepCnt) {
 				newPassengers.add(passQ.peek());
 				passQ.poll();
